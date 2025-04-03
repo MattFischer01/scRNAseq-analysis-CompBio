4ef535701a69103762c4d55413cc5d71e85736a8
Tyler Jensen <tjensen1115@gmail.com>
2025-04-02 21:03:07 -0500

    updated script to include making dir

--- code/fetch_fastq.sh
+++ code/fetch_fastq.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
 # check if an argument (file name) is provided
-if [ "$#" -ne 1 ]; then # if the number of arguments passed is not exactly 1
-    echo "Usage: $0 <srr_ids_file>" # print usage instructions ($0 is the script name)
-    exit 1 # exit script with error
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <srr_ids_file>"
+    exit 1
 fi
 
-SRR_FILE="$1" # assign first argument to the variable SRR_FILE
+# get the absolute path of the provided SRR text file
+SRR_FILE="$(realpath "$1")"
 
-mkdir -p SRR  # '-p' ensures no error if the directory already exists
+# define the output directory
+OUTPUT_DIR="$(dirname "$SRR_FILE")/scRNA_SRA"
 
-# read SRR IDs from the provided file and process them sequentially to ensure each is processed
+# create the output directory if it does not exist
+mkdir -p "$OUTPUT_DIR"
+
+# read SRR IDs from the provided file and process them 
 while read -r id; do
     echo "Processing $id..."
     
-    # run fasterq-dump and output files directly into the SRR directory
-    fasterq-dump "$id" -O SRR && gzip SRR/"$id"*.fastq  
+    # run fasterq-dump and output files directly into the scRNA_SRA directory
+    fasterq-dump "$id" -O "$OUTPUT_DIR" && gzip "$OUTPUT_DIR/$id"*.fastq  
 
-    if [ $? -eq 0 ]; then # loop states whether or not the file is processing
+    # this if then else statements are to echo whether or not the srr id is being processed
+    if [ $? -eq 0 ]; then
         echo "Successfully processed $id"
     else
         echo "Error processing $id"
